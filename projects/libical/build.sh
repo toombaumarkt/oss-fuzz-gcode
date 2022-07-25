2e34a00a17459d52ed2dd4693c9150231191dc7d
Tom Mittendorf <t.mittendorf@tu-braunschweig.de>
2022-07-25 16:59:51 +0200

    removed unnecessary projects

--- projects/libical/build.sh
+++
@@ -1,6 +0,0 @@
-cmake . -DSTATIC_ONLY=ON -DICAL_GLIB=False
-make install -j$(nproc)
-
-$CXX $CXXFLAGS -std=c++11 $SRC/libical_fuzzer.cc $LIB_FUZZING_ENGINE /usr/local/lib/libical.a -o $OUT/libical_fuzzer
-
-find . -name *.ics -print | zip -q $OUT/libical_fuzzer_seed_corpus.zip -@
