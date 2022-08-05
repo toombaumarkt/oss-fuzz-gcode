d85578aa06eca43bf4e72790e5bbfa51cb35adc5
Tom Mittendorf <t.mittendorf@tu-braunschweig.de>
2022-08-05 22:55:58 +0200

    added first candle files

---
+++ projects/candle/parser/wrapper.cpp
@@ -0,0 +1,42 @@
+#include <sstream>
+
+#include "wrapper.h"
+
+#include "gcodeparser.h"
+#include "gcodepreprocessorutils.h"
+#include "gcodeviewparse.h"
+
+
+bool SizeIsOk(size_t Size) {
+    return Size >= MIN_DATA_SIZE && Size <= MAX_DATA_SIZE;
+}
+
+std::stringstream ConvertByteDataToStringStream(const uint8_t *Data, size_t Size) {
+    char* fuzzed_input = new char[Size + 1];
+    memcpy(fuzzed_input, Data, Size);
+    fuzzed_input[Size] = '\0';
+    std::stringstream ss(fuzzed_input);
+    delete[]fuzzed_input;
+    return ss;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size){
+    if(! SizeIsOk(Size)) {
+        return 0;
+    }
+
+    std::stringstream ss = ConvertByteDataToStringStream(Data, Size);
+
+    GcodeViewParse gvp;
+
+    std::string line;
+    QList<QString> commands;
+
+    while(std::getline(ss, line)) {
+        commands.append(line.c_str());
+    }
+
+    gvp.toObjRedux(commands, 5, 5);
+
+    return 0;
+}
---
+++ projects/candle/parser/wrapper.h
@@ -0,0 +1,16 @@
+
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+
+// fuzzing parameters
+#define MAX_DATA_SIZE 1000
+#define MIN_DATA_SIZE 1
+#define DEBUG_OUTPUT 1
+
+
+#define false 0
+#define true 1
+
+
+#endif
